0bca39d8a0744d2e8daafbb7f8134f543bf9963c
yangyuxing <yangyuxing@socmap.net>
2019-08-22 15:32:15 +0800

    极验滑块破解版本v0.0.1

---
+++ cons.py
@@ -0,0 +1,46 @@
+index_url = 'https://www.taoguba.com.cn/index?blockID=1'
+
+chromedriver_path = "/Users/whoosy/Desktop/chromedriver"
+
+timeout = 100
+
+border = 7
+
+slider_class_name = "gt_slider_knob"
+
+login_button = "t_dblue"
+
+bg_filename = 'bg.jpg'
+
+full_bg_filename = 'full_bg.jpg'
+
+bg_class = 'gt_cut_bg_slice'
+
+full_bg_class = 'gt_cut_fullbg_slice'
+
+bg_url_re = 'url\\("(.*)"\\);'
+
+full_bg_url_re = 'url\\("(.*)"\\);'
+
+bg_location_re_x = 'background-position: (.*)px (.*)px;'
+
+bg_location_re_y = 'background-position: (.*)px (.*)px;'
+
+full_bg_location_re_x = 'background-position: (.*)px (.*)px;'
+
+full_bg_location_re_y = 'background-position: (.*)px (.*)px;'
+
+get_gap_left = 43
+
+is_pixel_equal_threshold = 60
+
+login_name_input_id = 'userPanelName'
+
+login_pwd_input_id = 'userPanelPwd'
+
+login_username = "***"
+
+login_pwd = "***"
+
+login_login_button_id = "loginBtn"
+
